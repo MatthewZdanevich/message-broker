29a05bcf3f46ba65a93dae53e90d945a8e893276
WoWich2005 <Lemyashevich2005@gmail.com>
2025-05-15 02:38:26 +0300

    Revert "Update broker.c"
    This reverts commit 7ac81fe3afb73c1c2efe33ce790322e518d1f939.

--- broker.c
+++ broker.c
@@ -16,13 +16,13 @@
 #define MAX_TOPIC_LEN 256
 #define QUEUE_SIZE 1000
 
-// Структура для сообщения (без изменений)
+// Структура для сообщения
 typedef struct {
     char data[MAX_MESSAGE_LEN];
     char topic[MAX_TOPIC_LEN];
 } Message;
 
-// Структура для очереди сообщений (без изменений)
+// Структура для очереди сообщений
 typedef struct {
     Message messages[QUEUE_SIZE];
     int head;
@@ -32,7 +32,7 @@ typedef struct {
     pthread_cond_t cond;
 } MessageQueue;
 
-// Структура для темы (без изменений)
+// Структура для темы
 typedef struct Topic {
     char name[MAX_TOPIC_LEN];
     Message messages[MAX_MESSAGES_PER_TOPIC];
@@ -40,13 +40,12 @@ typedef struct Topic {
     struct Topic *next;
 } Topic;
 
-// Структура для подписчика (добавлен флаг needs_flush)
+// Структура для подписчика
 typedef struct {
     int socket;
     char topic[MAX_TOPIC_LEN];
-    char send_buffer[MAX_MESSAGE_LEN * 10];
+    char send_buffer[MAX_MESSAGE_LEN * 10]; // Буфер для исходящих сообщений
     size_t send_buffer_len;
-    int needs_flush; // Флаг, указывающий, что есть данные для отправки
 } Subscriber;
 
 Topic *topics = NULL;
@@ -55,7 +54,7 @@ int subscriber_count = 0;
 MessageQueue message_queue;
 int epoll_fd;
 
-// Инициализация очереди (без изменений)
+// Инициализация очереди сообщений
 void init_message_queue() {
     message_queue.head = 0;
     message_queue.tail = 0;
@@ -64,12 +63,12 @@ void init_message_queue() {
     pthread_cond_init(&message_queue.cond, NULL);
 }
 
-// Добавление сообщения в очередь (без изменений)
+// Добавление сообщения в очередь
 int enqueue_message(const char *topic, const char *data) {
     pthread_mutex_lock(&message_queue.mutex);
     if (message_queue.count >= QUEUE_SIZE) {
         pthread_mutex_unlock(&message_queue.mutex);
-        return -1;
+        return -1; // Очередь полна
     }
     strncpy(message_queue.messages[message_queue.tail].topic, topic, MAX_TOPIC_LEN - 1);
     strncpy(message_queue.messages[message_queue.tail].data, data, MAX_MESSAGE_LEN - 1);
@@ -80,7 +79,7 @@ int enqueue_message(const char *topic, const char *data) {
     return 0;
 }
 
-// Извлечение сообщения из очереди (без изменений)
+// Извлечение сообщения из очереди
 int dequeue_message(Message *msg) {
     pthread_mutex_lock(&message_queue.mutex);
     while (message_queue.count == 0) {
@@ -93,13 +92,23 @@ int dequeue_message(Message *msg) {
     return 0;
 }
 
-// Установка неблокирующего режима (без изменений)
+// Установка неблокирующего режима для сокета
 void set_non_blocking(int sock) {
     int flags = fcntl(sock, F_GETFL, 0);
     fcntl(sock, F_SETFL, flags | O_NONBLOCK);
 }
 
-// Поиск или создание темы (без изменений)
+// Функция для удаления \n из строки
+void remove_newline(char *str, size_t len) {
+    for (size_t i = 0; i < len && str[i] != '\0'; i++) {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+            break;
+        }
+    }
+}
+
+// Поиск или создание темы
 Topic* find_or_create_topic(const char *name) {
     Topic *current = topics;
     while (current) {
@@ -116,35 +125,35 @@ Topic* find_or_create_topic(const char *name) {
     return new_topic;
 }
 
-// Сохранение сообщения (УБРАНО УДАЛЕНИЕ \n)
+// Сохранение сообщения
 void store_message(const char *topic_name, const char *message) {
     Topic *topic = find_or_create_topic(topic_name);
     if (topic->message_count < MAX_MESSAGES_PER_TOPIC) {
-        strncpy(topic->messages[topic->message_count].data, message, MAX_MESSAGE_LEN - 1);
-        topic->messages[topic->message_count].data[MAX_MESSAGE_LEN - 1] = '\0';
+        char clean_message[MAX_MESSAGE_LEN];
+        strncpy(clean_message, message, MAX_MESSAGE_LEN - 1);
+        clean_message[strcspn(clean_message, "\n")] = '\0';
+        strncpy(topic->messages[topic->message_count].data, clean_message, MAX_MESSAGE_LEN - 1);
         topic->message_count++;
     }
 }
 
-// Поток для обработки очереди (переработан для сохранения \n)
+// Поток для обработки очереди сообщений
 void* message_processor(void *arg) {
     Message msg;
     while (1) {
         dequeue_message(&msg);
         store_message(msg.topic, msg.data);
-
-        // Отправляем сообщение подписчикам как есть (без добавления \n)
+        char send_buffer[MAX_MESSAGE_LEN + 1]; // +1 для \n
+        snprintf(send_buffer, MAX_MESSAGE_LEN + 1, "%s\n", msg.data);
         for (int i = 0; i < subscriber_count; i++) {
             if (strcmp(subscribers[i].topic, msg.topic) == 0) {
-                size_t len = strlen(msg.data);
+                size_t len = strlen(send_buffer);
                 if (subscribers[i].send_buffer_len + len < sizeof(subscribers[i].send_buffer)) {
-                    memcpy(subscribers[i].send_buffer + subscribers[i].send_buffer_len, msg.data, len);
+                    memcpy(subscribers[i].send_buffer + subscribers[i].send_buffer_len, send_buffer, len);
                     subscribers[i].send_buffer_len += len;
-                    subscribers[i].needs_flush = 1;
-
                     // Добавляем сокет в epoll для записи
                     struct epoll_event ev;
-                    ev.events = EPOLLIN | EPOLLOUT | EPOLLET;
+                    ev.events = EPOLLIN | EPOLLOUT;
                     ev.data.fd = subscribers[i].socket;
                     epoll_ctl(epoll_fd, EPOLL_CTL_MOD, subscribers[i].socket, &ev);
                 }
@@ -179,7 +188,7 @@ int main() {
 
     // Создаём epoll
     epoll_fd = epoll_create1(0);
-    ev.events = EPOLLIN | EPOLLET;
+    ev.events = EPOLLIN;
     ev.data.fd = server_fd;
     epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev);
 
@@ -194,14 +203,14 @@ int main() {
                 socklen_t client_len = sizeof(client_addr);
                 int client_socket = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
                 set_non_blocking(client_socket);
-                ev.events = EPOLLIN | EPOLLET;
+                ev.events = EPOLLIN;
                 ev.data.fd = client_socket;
                 epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &ev);
                 printf("New client connected: %d\n", client_socket);
             } else {
                 int client_socket = events[i].data.fd;
                 if (events[i].events & EPOLLIN) {
-                    // Чтение данных от клиента
+                    // Данные от клиента
                     char buffer[MAX_MESSAGE_LEN];
                     int bytes_read = read(client_socket, buffer, sizeof(buffer) - 1);
                     if (bytes_read <= 0) {
@@ -218,16 +227,11 @@ int main() {
                         continue;
                     }
                     buffer[bytes_read] = '\0';
+                    remove_newline(buffer, bytes_read);
 
-                    // Обработка команд (без удаления \n)
                     if (strncmp(buffer, "SUBSCRIBE ", 10) == 0) {
+                        // Регистрируем подписку
                         char *topic = buffer + 10;
-                        // Удаляем \n в конце команды (только для команды!)
-                        size_t len = strlen(topic);
-                        if (len > 0 && topic[len - 1] == '\n') {
-                            topic[len - 1] = '\0';
-                        }
-
                         int already_subscribed = 0;
                         for (int j = 0; j < subscriber_count; j++) {
                             if (subscribers[j].socket == client_socket) {
@@ -238,23 +242,22 @@ int main() {
                         if (!already_subscribed && subscriber_count < MAX_SUBSCRIBERS) {
                             subscribers[subscriber_count].socket = client_socket;
                             subscribers[subscriber_count].send_buffer_len = 0;
-                            subscribers[subscriber_count].needs_flush = 0;
                             strncpy(subscribers[subscriber_count].topic, topic, MAX_TOPIC_LEN - 1);
                             subscriber_count++;
                             char *response = "Subscribed\n";
                             send(client_socket, response, strlen(response), 0);
                             printf("Client %d subscribed to %s\n", client_socket, topic);
 
-                            // Отправляем историю сообщений
+                            // Отправляем сохранённые сообщения
                             Topic *topic_ptr = find_or_create_topic(topic);
                             for (int j = 0; j < topic_ptr->message_count; j++) {
-                                size_t len = strlen(topic_ptr->messages[j].data);
+                                char send_buffer[MAX_MESSAGE_LEN + 1]; // +1 для \n
+                                snprintf(send_buffer, MAX_MESSAGE_LEN + 1, "%s\n", topic_ptr->messages[j].data);
+                                size_t len = strlen(send_buffer);
                                 if (subscribers[subscriber_count - 1].send_buffer_len + len < sizeof(subscribers[subscriber_count - 1].send_buffer)) {
-                                    memcpy(subscribers[subscriber_count - 1].send_buffer + subscribers[subscriber_count - 1].send_buffer_len, 
-                                           topic_ptr->messages[j].data, len);
+                                    memcpy(subscribers[subscriber_count - 1].send_buffer + subscribers[subscriber_count - 1].send_buffer_len, send_buffer, len);
                                     subscribers[subscriber_count - 1].send_buffer_len += len;
-                                    subscribers[subscriber_count - 1].needs_flush = 1;
-                                    ev.events = EPOLLIN | EPOLLOUT | EPOLLET;
+                                    ev.events = EPOLLIN | EPOLLOUT;
                                     ev.data.fd = client_socket;
                                     epoll_ctl(epoll_fd, EPOLL_CTL_MOD, client_socket, &ev);
                                 }
@@ -263,29 +266,29 @@ int main() {
                             send(client_socket, "Already subscribed or too many subscribers\n", 42, 0);
                         }
                     } else if (strncmp(buffer, "PUBLISH ", 8) == 0) {
-                        char *topic_start = buffer + 8;
-                        char *topic_end = strchr(topic_start, ' ');
-                        if (topic_end) {
-                            *topic_end = '\0';
-                            char *message = topic_end + 1;
-                            enqueue_message(topic_start, message); // Сохраняем сообщение как есть
-                            printf("Queued message for %s: %s\n", topic_start, message);
+                        // Публикуем сообщение асинхронно
+                        char *topic = strtok(buffer + 8, " ");
+                        char *message = strtok(NULL, "");
+                        if (topic && message) {
+                            enqueue_message(topic, message);
+                            printf("Queued message for %s: %s\n", topic, message);
                         }
                     } else {
                         send(client_socket, "Unknown command\n", 16, 0);
                     }
                 }
                 if (events[i].events & EPOLLOUT) {
-                    // Отправка данных подписчикам
+                    // Отправка данных из буфера
                     for (int j = 0; j < subscriber_count; j++) {
-                        if (subscribers[j].socket == client_socket && subscribers[j].needs_flush) {
+                        if (subscribers[j].socket == client_socket && subscribers[j].send_buffer_len > 0) {
                             ssize_t sent = send(client_socket, subscribers[j].send_buffer, subscribers[j].send_buffer_len, 0);
                             if (sent > 0) {
                                 memmove(subscribers[j].send_buffer, subscribers[j].send_buffer + sent, subscribers[j].send_buffer_len - sent);
                                 subscribers[j].send_buffer_len -= sent;
                                 if (subscribers[j].send_buffer_len == 0) {
-                                    subscribers[j].needs_flush = 0;
-                                    ev.events = EPOLLIN | EPOLLET;
+                                    // Убираем EPOLLOUT, если буфер пуст
+                                    struct epoll_event ev;
+                                    ev.events = EPOLLIN;
                                     ev.data.fd = client_socket;
                                     epoll_ctl(epoll_fd, EPOLL_CTL_MOD, client_socket, &ev);
                                 }
